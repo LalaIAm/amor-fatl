2ba7f134884e9170e98ae1745672a2ed6ff4b68e
Lauren Thorud <lthorud12@gmail.com>
2025-12-13 09:44:13 -0600

    add cypress

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: "mu29g6",
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
