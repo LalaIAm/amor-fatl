cbaa55dc63e959b118794d7bb7002367fb4ff8a3
Lauren Thorud <lthorud12@gmail.com>
2025-12-13 14:25:17 -0600

    add fire

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,37 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   projectId: "mu29g6",
+  reporter: "mocha-multi-reporters",
+  reporterOptions: {
+    reporterEnabled: "cypress-mochawesome-reporter, mocha-junit-reporter",
+    mochaJunitReporterReporterOptions: {
+      mochaFile: "cypress/reports/junit/test-results-[hash].xml",
+      toConsole: true,
+      testsuitesTitle: "Cypress Tests",
+      suiteTitleSeparatedBy: " - ",
+      includePending: true,
+    },
+  },
+  env: {
+    codeCoverage: {
+      exclude: "cypress/**/*.*",
+    },
+  },
   e2e: {
+    baseUrl: "http://localhost:5173",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+    setupNodeEvents(on, config) {
+      // eslint-disable-next-line no-undef
+      require("@cypress/code-coverage/task")(on, config);
+      return config;
     },
   },
 });
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import istanbul from "vite-plugin-istanbul";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    istanbul({
+      cypress: true,
+      requireEnv: false,
+    }),
+  ],
+});
